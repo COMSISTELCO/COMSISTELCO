2e30a035496fa8cc702a101836d4e8687d7c81b5
COMSISTELCO <coordinadorit@comsistelco.com>
2026-02-18 17:09:20 -0500

    Update video2.js

--- js/video2.js
+++ js/video2.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('toggleVideoBtn');
 const container = document.getElementById('videoContainer');
 const iframe = document.getElementById('ytIframe');
-const videoSrc = "https://www.youtube.com/watch?v=KyFScU_-t6c";
+const videoSrc = "https://www.youtube.com/embed/KyFScU_-t6c?si=ctoOC44gkxzg3FmT";
 
 btn.addEventListener('click', () => {
     if(container.classList.contains('show-video')) {
@@ -14,4 +14,5 @@ btn.addEventListener('click', () => {
         iframe.src = videoSrc + "?autoplay=1&mute=1"; // autoplay al mostrar
     }
 });
+
 
